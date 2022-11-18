88734a43c4c4dacab8c92fa4fa626828b440c9c9
k.golikov <kgolikov@37bytes.com>
2022-11-18 22:08:39 +0500

    Update something

--- src/main/java/ru/mrgrd56/api/ApiMrgrd56Application.java
+++ src/main/java/ru/mrgrd56/api/ApiMrgrd56Application.java
@@ -1,20 +1,23 @@
 package ru.mrgrd56.api;
 
-import nu.pattern.OpenCV;
-import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ApiMrgrd56Application {
-
-    static {
-//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        OpenCV.loadLocally();
+    public ApiMrgrd56Application(Environment environment) {
+        var activeProfiles = List.of(environment.getActiveProfiles());
+        if (activeProfiles.contains("prod")) {
+            System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
+        } else {
+            nu.pattern.OpenCV.loadLocally();
+        }
     }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiMrgrd56Application.class, args);
     }
-
 }
