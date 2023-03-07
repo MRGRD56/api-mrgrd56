5cfc83d685575f60c4a83e7be93c1d119d63953b
k.golikov <kgolikov@37bytes.com>
2023-03-08 00:21:52 +0500

    Changes on GET /ip

--- src/main/java/ru/mrgrd56/api/controllers/HomeController.java
+++ src/main/java/ru/mrgrd56/api/controllers/HomeController.java
@@ -4,25 +4,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
-import java.util.Map;
 
 @RestController
 public class HomeController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("ip")
-    public String getIpAddress(
-            @RequestHeader(value = "X-Real-IP", required = false) String realIp,
-            @RequestHeader Map<String, String> headers,
-            HttpServletRequest request) {
-        log.info("/ip headers {}", headers);
-        log.info("/ip header X-Real-IP {}", request.getHeader("X-Real-IP"));
-
+    public String getIpAddress(HttpServletRequest request) {
+        String realIp = request.getHeader("X-Real-IP");
         if (StringUtils.isNotBlank(realIp)) {
             return realIp;
         }
