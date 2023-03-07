9ed4d874879ba206de9ce3e82de1b974a18f9aeb
k.golikov <kgolikov@37bytes.com>
2023-03-08 00:17:47 +0500

    Add logs on GET /ip

--- src/main/java/ru/mrgrd56/api/controllers/HomeController.java
+++ src/main/java/ru/mrgrd56/api/controllers/HomeController.java
@@ -1,6 +1,10 @@
 package ru.mrgrd56.api.controllers;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,8 +12,19 @@ import java.time.Instant;
 
 @RestController
 public class HomeController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("ip")
-    public String getIpAddress(HttpServletRequest request) {
+    public String getIpAddress(
+            @RequestHeader(value = "X-Real-IP", required = false) String realIp,
+            HttpServletRequest request) {
+        log.info("/ip headers {}", request.getHeaderNames());
+        log.info("/ip header X-Real-IP {}", request.getHeaders("X-Real-IP"));
+
+        if (StringUtils.isNotBlank(realIp)) {
+            return realIp;
+        }
+
         return request.getRemoteAddr();
     }
 
